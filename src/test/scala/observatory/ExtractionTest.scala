e06998e2c5801d274658a9bb8cda195564e54956
remusdecea <remus.decea@gmail.com>
2017-04-15 01:02:00 +0300

    Wrote ExtractionTest

--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import java.time.LocalDate
+
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -7,5 +9,15 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExtractionTest extends FunSuite {
 
-  
+  test("locate temperatures should list all records in Celsius with date and location"){
+    val temperatures = Extraction.locateTemperatures(2015, "stations.csv", "test.csv")
+
+    val expectedTemperatutes = Seq(
+      (LocalDate.of(2015, 8, 11), Location(37.35, -78.433), 27.3),
+      (LocalDate.of(2015, 12, 6), Location(37.358, -78.438), 0.0),
+      (LocalDate.of(2015, 1, 29), Location(37.358, -78.438), 2.0)
+    )
+
+    assert(temperatures == expectedTemperatutes)
+  }
 }
